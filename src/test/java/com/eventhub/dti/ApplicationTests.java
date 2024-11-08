5bc1e7973c3e3979204a33729591c96883f29fff
Erlangga15 <angga.hendrika@gmail.com>
2024-11-08 22:00:33 +0700

    feat: initial commit

---
+++ src/test/java/com/eventhub/dti/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eventhub.dti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // Test will pass if Spring context loads successfully
+  }
+
+}
